4e7f4b747f669e923b8e6fb12f966cedcbc9aa23
daviidy <yaodavidarmel@gmail.com>
2018-07-24 07:33:23 +0000

    vendor changed

--- resources/views/layouts/header-facture.blade.php
+++ resources/views/layouts/header-facture.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<title>Minaci 2018 | @yield('title')</title>
+	<title>Minaci 2018 | @yield('title')</title> 
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 <!--===============================================================================================-->
@@ -19,6 +19,8 @@
 <!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="facture/css/util.css">
 	<link rel="stylesheet" type="text/css" href="facture/css/main.css">
+
+	<link rel="stylesheet" href="/edu/style2.css">
 <!--===============================================================================================-->
 </head>
 <body>
